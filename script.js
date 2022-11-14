ee461f0ff893fa093d7bb9fd02c8066da6dc18bf
Muhammed Aisha <aishatayinke9@gmail.com>
2022-11-14 02:43:30 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let readMore = document.querySelector(".read-more");
+let aboutBtn = document.querySelector(".about-btn");
+
+
+aboutBtn.addEventListener('click', function() {
+    readMore.style.display = 'grid'
+    aboutBtn.style.display = 'none'
+   
+
+})
